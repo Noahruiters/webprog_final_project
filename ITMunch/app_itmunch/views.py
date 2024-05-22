1de64fade5e5315d4eed15e2c4888dea0a35e8e2
th7n <royer.than@gmail.com>
2024-05-22 21:19:06 +0900

    user login register logout

--- ITMunch/app_itmunch/views.py
+++ ITMunch/app_itmunch/views.py
@@ -1,14 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 def index(request):
+    if not request.user.is_authenticated:
+        return redirect('app_itmunch:login')
     return render(request, 'app_itmunch/index.html')
 
 def login_view(request):
-    return HttpResponse("Hello World!")
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('app_itmunch:index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'app_itmunch/login.html', {'form': form})
 
 def register_view(request):
-    return HttpResponse("Hello World!")
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('app_itmunch:login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'app_itmunch/register.html', {'form': form})
 
 def logout_view(request):
-    return HttpResponse("Hello World!")
+    logout(request)
+    return redirect('app_itmunch:login')
