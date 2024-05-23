7f20eb8e9cf5762ec4dac63d3ad7974a734d7144
th7n <royer.than@gmail.com>
2024-05-23 10:58:18 +0900

    added calorie estimation logic

--- ITMunch/app_itmunch/views.py
+++ ITMunch/app_itmunch/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django import forms
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
@@ -14,7 +15,8 @@ def index(request):
         return redirect('app_itmunch:login')
     user = request.user
     profile, created = Profile.objects.get_or_create(user=user)
-    return render(request, 'app_itmunch/index.html', {'profile': profile})
+    calories = calculate_calories(profile)
+    return render(request, 'app_itmunch/index.html', {'profile': profile, 'calories': calories})
 
 def login_view(request):
     if request.method == 'POST':
@@ -59,3 +61,26 @@ def questions_view(request):
     else:
         form = ProfileForm(instance=profile)
     return render(request, 'app_itmunch/questions.html', {'form': form})
+
+def calculate_calories(profile):
+    weight = float(profile.current_weight)
+    height = float(profile.height)
+    age = (datetime.date.today() - profile.birthday).days // 365
+    if profile.gender == 'male':
+        bmr = 10 * weight + 6.25 * height - 5 * age + 5
+    else:
+        bmr = 10 * weight + 6.25 * height - 5 * age - 161
+    
+    multiplier = {
+        'sedentary': 1.2,
+        'lightly_active': 1.375,
+        'moderately_active': 1.55,
+        'very_active': 1.725,
+        'extra_active': 1.9
+    }
+    calories = int(bmr * multiplier[profile.activity])
+    if profile.goal == 'lose_weight':
+        calories -= 500
+    elif profile.goal == 'gain_weight':
+        calories += 500
+    return calories
