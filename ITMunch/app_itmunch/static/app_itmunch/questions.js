b515c8163a45a2cc173c5d3d3e809e3005e84101
Alicia <alicia.vial@ecam.fr>
2024-05-31 09:14:20 +0200

    Uploaded script -still errors (seems like the scrip is not read)

--- ITMunch/app_itmunch/static/app_itmunch/questions.js
+++ ITMunch/app_itmunch/static/app_itmunch/questions.js
@@ -1,28 +1,38 @@
 document.addEventListener('DOMContentLoaded',function(){
+
+    console.log("script loaded");
+
     const questions = document.querySelectorAll('.question');
-    const nextButton = document.getElementById('next_button');
-    const submitButton = document.getElementById('submit_button');
+    const nextButton = document.querySelector('#button_next');
+    const submitButton = document.querySelector('#button_submit');
     let currentQuestionIndex = 0;
 
-    nextButton.onclick(function(){
+
+    nextButton.onclick=function(){
+        console.log("button clicked");
         const currentQuestion=questions[currentQuestionIndex];
+        //if the question is not answered, display an alert message
         if (input && input.value.trim() === '') {
-            alert('Please answer the question before proceeding.');
+            alert('Please answer the question');
             return;
         }
 
+        //change the active question to the next field
         currentQuestion.classList.remove('active');
         currentQuestionIndex++;
         if (currentQuestionIndex < questions.length) {
             questions[currentQuestionIndex].classList.add('active');
         }
 
+        //for the last question, make the button submit active, and next inactive
         if (currentQuestionIndex === questions.length - 1) {
             nextButton.style.display = 'none';
             submitButton.style.display = 'inline-block';
         }
 
-    })
+        //questions[0].querySelector('input, select, textarea').focus();
+
+    }
 
 
 
